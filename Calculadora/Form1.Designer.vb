b35f04360ee6bddbed900a2f7b593885b4fd19e0
Angel Cedillo <80221251+R0B0T023@users.noreply.github.com>
2021-06-21 14:01:28 -0500

    Add files via upload

---
+++ Calculadora/Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtpantalla = New System.Windows.Forms.TextBox()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnpunto = New System.Windows.Forms.Button()
+        Me.btnborrar = New System.Windows.Forms.Button()
+        Me.btnigual = New System.Windows.Forms.Button()
+        Me.btnmulti = New System.Windows.Forms.Button()
+        Me.btndiv = New System.Windows.Forms.Button()
+        Me.btnrest = New System.Windows.Forms.Button()
+        Me.btnsuma = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtpantalla
+        '
+        Me.txtpantalla.Location = New System.Drawing.Point(13, 13)
+        Me.txtpantalla.Multiline = True
+        Me.txtpantalla.Name = "txtpantalla"
+        Me.txtpantalla.Size = New System.Drawing.Size(317, 31)
+        Me.txtpantalla.TabIndex = 0
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(12, 62)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(75, 23)
+        Me.btn1.TabIndex = 1
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(93, 62)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(75, 23)
+        Me.btn2.TabIndex = 2
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(174, 62)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(75, 23)
+        Me.btn3.TabIndex = 3
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(13, 91)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(75, 23)
+        Me.btn4.TabIndex = 4
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(94, 91)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(75, 23)
+        Me.btn5.TabIndex = 5
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(175, 91)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(75, 23)
+        Me.btn6.TabIndex = 6
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(13, 120)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(75, 23)
+        Me.btn7.TabIndex = 7
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(94, 120)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(75, 23)
+        Me.btn8.TabIndex = 8
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(175, 120)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(75, 23)
+        Me.btn9.TabIndex = 9
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(93, 149)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(75, 23)
+        Me.btn0.TabIndex = 10
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnpunto
+        '
+        Me.btnpunto.Location = New System.Drawing.Point(13, 149)
+        Me.btnpunto.Name = "btnpunto"
+        Me.btnpunto.Size = New System.Drawing.Size(75, 23)
+        Me.btnpunto.TabIndex = 11
+        Me.btnpunto.Text = "."
+        Me.btnpunto.UseVisualStyleBackColor = True
+        '
+        'btnborrar
+        '
+        Me.btnborrar.Location = New System.Drawing.Point(174, 149)
+        Me.btnborrar.Name = "btnborrar"
+        Me.btnborrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnborrar.TabIndex = 12
+        Me.btnborrar.Text = "C"
+        Me.btnborrar.UseVisualStyleBackColor = True
+        '
+        'btnigual
+        '
+        Me.btnigual.Location = New System.Drawing.Point(13, 178)
+        Me.btnigual.Name = "btnigual"
+        Me.btnigual.Size = New System.Drawing.Size(233, 23)
+        Me.btnigual.TabIndex = 13
+        Me.btnigual.Text = "="
+        Me.btnigual.UseVisualStyleBackColor = True
+        '
+        'btnmulti
+        '
+        Me.btnmulti.Location = New System.Drawing.Point(255, 62)
+        Me.btnmulti.Name = "btnmulti"
+        Me.btnmulti.Size = New System.Drawing.Size(75, 23)
+        Me.btnmulti.TabIndex = 14
+        Me.btnmulti.Text = "*"
+        Me.btnmulti.UseVisualStyleBackColor = True
+        '
+        'btndiv
+        '
+        Me.btndiv.Location = New System.Drawing.Point(255, 91)
+        Me.btndiv.Name = "btndiv"
+        Me.btndiv.Size = New System.Drawing.Size(75, 23)
+        Me.btndiv.TabIndex = 15
+        Me.btndiv.Text = "/"
+        Me.btndiv.UseVisualStyleBackColor = True
+        '
+        'btnrest
+        '
+        Me.btnrest.Location = New System.Drawing.Point(256, 120)
+        Me.btnrest.Name = "btnrest"
+        Me.btnrest.Size = New System.Drawing.Size(75, 23)
+        Me.btnrest.TabIndex = 16
+        Me.btnrest.Text = "-"
+        Me.btnrest.UseVisualStyleBackColor = True
+        '
+        'btnsuma
+        '
+        Me.btnsuma.Location = New System.Drawing.Point(256, 149)
+        Me.btnsuma.Name = "btnsuma"
+        Me.btnsuma.Size = New System.Drawing.Size(75, 52)
+        Me.btnsuma.TabIndex = 17
+        Me.btnsuma.Text = "+"
+        Me.btnsuma.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(342, 215)
+        Me.Controls.Add(Me.btnsuma)
+        Me.Controls.Add(Me.btnrest)
+        Me.Controls.Add(Me.btndiv)
+        Me.Controls.Add(Me.btnmulti)
+        Me.Controls.Add(Me.btnigual)
+        Me.Controls.Add(Me.btnborrar)
+        Me.Controls.Add(Me.btnpunto)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.txtpantalla)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtpantalla As TextBox
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnpunto As Button
+    Friend WithEvents btnborrar As Button
+    Friend WithEvents btnigual As Button
+    Friend WithEvents btnmulti As Button
+    Friend WithEvents btndiv As Button
+    Friend WithEvents btnrest As Button
+    Friend WithEvents btnsuma As Button
+End Class
---
+++ Calculadora/Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+    Public num1 As Double
+    Public operacion As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txtpantalla.Text = txtpantalla.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txtpantalla.Text = txtpantalla.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txtpantalla.Text = txtpantalla.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        txtpantalla.Text = txtpantalla.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txtpantalla.Text = txtpantalla.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txtpantalla.Text = txtpantalla.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txtpantalla.Text = txtpantalla.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txtpantalla.Text = txtpantalla.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txtpantalla.Text = txtpantalla.Text + "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txtpantalla.Text = txtpantalla.Text + "0"
+    End Sub
+
+    Private Sub btnpunto_Click(sender As Object, e As EventArgs) Handles btnpunto.Click
+        txtpantalla.Text = txtpantalla.Text + "."
+    End Sub
+
+    Private Sub btnborrar_Click(sender As Object, e As EventArgs) Handles btnborrar.Click
+        txtpantalla.Text = ""
+    End Sub
+
+    Private Sub btnsuma_Click(sender As Object, e As EventArgs) Handles btnsuma.Click
+        num1 = CDbl(txtpantalla.Text)
+        txtpantalla.Text = ""
+        operacion = 1
+    End Sub
+
+    Private Sub btnrest_Click(sender As Object, e As EventArgs) Handles btnrest.Click
+        num1 = CDbl(txtpantalla.Text)
+        txtpantalla.Text = ""
+        operacion = 2
+    End Sub
+
+    Private Sub btndiv_Click(sender As Object, e As EventArgs) Handles btndiv.Click
+        num1 = CDbl(txtpantalla.Text)
+        txtpantalla.Text = ""
+        operacion = 3
+    End Sub
+
+    Private Sub btnmulti_Click(sender As Object, e As EventArgs) Handles btnmulti.Click
+        num1 = CDbl(txtpantalla.Text)
+        txtpantalla.Text = ""
+        operacion = 4
+    End Sub
+
+    Private Sub btnigual_Click(sender As Object, e As EventArgs) Handles btnigual.Click
+        If (operacion = 1) Then
+            txtpantalla.Text = num1 + CDbl(txtpantalla.Text)
+
+        ElseIf (operacion = 2) Then
+            txtpantalla.Text = num1 - CDbl(txtpantalla.Text)
+
+        ElseIf (operacion = 3) Then
+            txtpantalla.Text = num1 / CDbl(txtpantalla.Text)
+
+        ElseIf (operacion = 4) Then
+            txtpantalla.Text = num1 * CDbl(txtpantalla.Text)
+
+        End If
+    End Sub
+End Class
